82464b0be8c4b0a68d4ed6b2605b7c4f8891d9f0
SaipradeepR <53404427+SaipradeepR@users.noreply.github.com>
2024-11-15 10:38:46 +0530

    4.8.19 dev v5 (#39)
    * code changes for all partner data saving in common table (#37)
    
    * contentSearchTags Payload validation issue resolved.(KB-7194) issue.
    
    * code changes for all partner data saving in common table
    
    * code changes for all partner data saving in common table
    
    * code changes for all partner data saving in common table
    
    * code changes for all partner data saving in common table
    
    * null check added for fileValidation (#38)
    
    * contentSearchTags Payload validation issue resolved.(KB-7194) issue.
    
    * code changes for all partner data saving in common table
    
    * code changes for all partner data saving in common table
    
    * code changes for all partner data saving in common table
    
    * code changes for all partner data saving in common table
    
    * null check added for fileValidation
    
    ---------
    
    Co-authored-by: Arpitha <arpitha.sureshappa@tarento.com>

--- src/main/java/com/igot/cios/consumer/OnboardContentConsumer.java
+++ src/main/java/com/igot/cios/consumer/OnboardContentConsumer.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.igot.cios.exception.CiosContentException;
-import com.igot.cios.plugins.ContentPartnerPluginService;
-import com.igot.cios.plugins.ContentSource;
 import com.igot.cios.plugins.DataTransformUtility;
-import com.igot.cios.plugins.config.ContentPartnerServiceFactory;
 import com.igot.cios.service.impl.CiosContentServiceImpl;
 import com.igot.cios.util.Constants;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
@@ -30,12 +26,9 @@ public class OnboardContentConsumer {
     DataTransformUtility dataTransformUtility;
 
     @Autowired
-    private ContentPartnerServiceFactory contentPartnerServiceFactory;
-
-    @Autowired
     private CiosContentServiceImpl ciosContentServiceimpl;
 
-    @KafkaListener(topics = "${kafka.topic.content.onboarding}", groupId = "enrollment-progress")
+    @KafkaListener(topics = "${kafka.topic.content.onboarding}", groupId = "${content.onboarding.consumer.group}")
     public void consumeMessage(String message) {
         String partnerCode = null;
         String partnerId = null;
@@ -43,7 +36,6 @@ public class OnboardContentConsumer {
         Timestamp initiatedOn = null;
         String fileId = null;
         String loadContentErrorMessage = null;
-
         try {
             log.info("Consuming the content to onboard in cios");
             Map<String, Object> receivedMessage = objectMapper.readValue(message, new TypeReference<Map<String, Object>>() {});
@@ -57,7 +49,8 @@ public class OnboardContentConsumer {
             log.info("Received {} records from Kafka", receivedMessage.size());
             List<Map<String, String>> processedData = objectMapper.convertValue(receivedMessage.get("data"), new TypeReference<List<Map<String, String>>>() {});
             try {
-                processReceivedData(partnerCode, processedData, fileName, fileId, initiatedOn);
+                //ciosContentServiceimpl.processRowsAndCreateLogs(processedData, partnerId, fileId, fileName, initiatedOn,partnerCode, loadContentErrorMessage);
+                processReceivedData(partnerCode, processedData, fileName, fileId, initiatedOn,partnerId);
             }catch (Exception e){
                 loadContentErrorMessage = "Error in processReceivedData: " + e.getMessage();
                 log.error(loadContentErrorMessage);
@@ -71,18 +64,18 @@ public class OnboardContentConsumer {
     }
 
 
-    private void processReceivedData(String partnerCode, List<Map<String, String>> processedData, String fileName, String fileId, Timestamp initiatedOn) throws IOException {
+    private void processReceivedData(String partnerCode, List<Map<String, String>> processedData, String fileName, String fileId, Timestamp initiatedOn,String partnerId) throws IOException {
         log.info("Processing {} records for partner code {}", processedData.size(), partnerCode);
         JsonNode jsonData = objectMapper.valueToTree(processedData);
 
         JsonNode entity = dataTransformUtility.fetchPartnerInfoUsingApi(partnerCode);
         List<Object> contentJson = objectMapper.convertValue(entity.path("result").path("trasformContentJson"), new TypeReference<List<Object>>() {
         });
-        if (contentJson == null || contentJson.isEmpty()) {
-            throw new CiosContentException("Transformation data not present in content partner db", HttpStatus.INTERNAL_SERVER_ERROR);
+        if(contentJson == null || contentJson.isEmpty()){
+            log.error("trasformContentJson is missing, please update in contentPartner");
+            throw new CiosContentException("ERROR","trasformContentJson is missing, please update in contentPartner", HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        ContentPartnerPluginService service = contentPartnerServiceFactory.getContentPartnerPluginService(ContentSource.fromPartnerCode(partnerCode));
-        service.loadContentFromExcel(jsonData, partnerCode, fileName, fileId, contentJson);
+        dataTransformUtility.updateProcessedDataInDb(jsonData, partnerCode, fileName, fileId, contentJson,partnerId);
     }
 
 }
